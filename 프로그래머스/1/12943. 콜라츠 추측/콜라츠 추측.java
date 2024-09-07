1f7bb876ab1e97361ca975df7c347ea273d844c3
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-07 11:23:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,11 +1,10 @@
 class Solution {
-    public long solution(long num) {
+    public int solution(long num) {
         int cnt = 0;
         
         while (cnt != 500) {
             if (num == 1) return cnt;
-            else if (num % 2 == 0) num /= 2;
-            else num = num * 3 + 1;
+            num = (num % 2 == 0) ? num / 2 : num * 3 + 1;
 
             cnt++;
         }
