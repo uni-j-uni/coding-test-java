8e783ec06384b1efcddb4ebe81518843274a19fa
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-07 11:54:03 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 45;
+        
+        for (int num : numbers) {
+            sum -= num;
+        }
+        
+        return sum;
+    }
+}
