33b190eace8d76fbe0567000993b1cbb0b49120c
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-07 11:45:32 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        
+        for (int i = 0; i < absolutes.length; i++) {
+            sum += signs[i] ? absolutes[i] : -absolutes[i];
+        }
+        
+        return sum;
+    }
+}
