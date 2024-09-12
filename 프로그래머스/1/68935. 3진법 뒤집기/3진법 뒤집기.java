3159094cfd42031389d0390fe3c0c94a16d5cfef
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-12 23:19:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.13 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,19 +1,12 @@
 class Solution {
     public int solution(int n) {
         StringBuilder sb = new StringBuilder();
-        int answer = 0;
 
-        while (n > 0) {
+        while(n > 0) {
             sb.append(n % 3);
             n /= 3;
         }
-        
-        sb.reverse();
 
-        for (int i = 0; i < sb.length(); i++) {
-            answer += (int) (Math.pow(3, i) * (sb.charAt(i) - 48));
-        }
-
-        return answer;
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935#) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 0.09 ms
+메모리: 73.6 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 12일 23:13:54
+2024년 09월 12일 23:19:16
 
 ### 문제 설명
 
