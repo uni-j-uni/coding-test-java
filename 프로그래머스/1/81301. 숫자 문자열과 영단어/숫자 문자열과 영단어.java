617094bd6a3b0d5029eee5fbb69293c6ee44503b
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-16 15:11:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.74 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String s) {
+        if (s.contains("zero")) s = s.replaceAll("zero", "0");
+        if (s.contains("one")) s = s.replaceAll("one", "1");
+        if (s.contains("two")) s = s.replaceAll("two", "2");
+        if (s.contains("three")) s = s.replaceAll("three", "3");
+        if (s.contains("four")) s = s.replaceAll("four", "4");
+        if (s.contains("five")) s = s.replaceAll("five", "5");
+        if (s.contains("six")) s = s.replaceAll("six", "6");
+        if (s.contains("seven")) s = s.replaceAll("seven", "7");
+        if (s.contains("eight")) s = s.replaceAll("eight", "8");
+        if (s.contains("nine")) s = s.replaceAll("nine", "9");
+
+        return Integer.parseInt(s);
+    }
+}
