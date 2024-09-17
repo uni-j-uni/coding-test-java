296fd1556554b1d5daa7a6ec1e89f3a7edd5ab23
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-17 19:27:43 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.06 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,15 +3,11 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for (int i = 1; i < food.length; i++) {
-            for (int j = 0; j < food[i] / 2; j++) {
-                sb.append(i);
-            }
+            sb.append(String.valueOf(i).repeat(food[i] / 2));
         }
         
-        String place = sb.toString();
-        sb.append(0).reverse();
+        String place = sb + "0";
         
-        
-        return place + sb.toString();
+        return place + sb.reverse();
     }
 }
