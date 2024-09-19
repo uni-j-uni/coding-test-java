b2736f8f5e79cdeee5e9ee5bee49bd77ee9be4b1
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-19 22:08:16 +0900

    [level 1] Title: 추억 점수, Time: 24.21 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        int index = 0;
 
         for (int i = 0; i < photo.length; i++) {
             for (int j = 0; j < photo[i].length; j++) {
                 for (int k = 0; k < name.length; k++) {
                     if (photo[i][j].equals(name[k])) {
-                        answer[index] += yearning[k];
+                        answer[i] += yearning[k];
                     }
                 }
             }
-            index++;
         }
 
         return answer;
