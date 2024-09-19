76a21cdf6b529972d222469d6e24f73852038ffd
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-20 00:07:52 +0900

    [level 1] Title: 폰켓몬, Time: 6.70 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        return Math.min(set.size(), nums.length / 2);
+    }
+}
