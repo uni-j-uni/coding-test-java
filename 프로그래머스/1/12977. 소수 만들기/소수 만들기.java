9c0f3541262744b9155cd61731bf625fbafc7f28
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-21 16:58:18 +0900

    [level 1] Title: 소수 만들기, Time: 7.99 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = nums.length * (nums.length - 1) * (nums.length - 2) / 6 ;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int sum = 0;
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    for (int l = 2; l <= Math.sqrt(nums[i] + nums[j] + nums[k]); l++) {
+                        if ((nums[i] + nums[j] + nums[k]) % l  == 0) {
+                            answer -= 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
