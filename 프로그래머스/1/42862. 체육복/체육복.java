0158f869e07e56acbba112db0043a9d8b27091b8
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-09-29 16:26:31 +0900

    [level 1] Title: 체육복, Time: 0.57 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         Arrays.sort(lost);
-        Arrays.sort(reserve);
-
+        Arrays.sort(reserve);        
+        
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
                 if (lost[i] == reserve[j]) {
