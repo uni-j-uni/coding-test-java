2f2e4dfa6700ef14d236db66badb56b8b5969afd
HeeJun Yoon <unijun0109@gmail.com>
2025-01-13 23:00:30 +0900

    [level 2] Title: 전화번호 목록, Time: 389.86 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            for (int j = i + 1; j < phone_book.length; j++) {
+                if (phone_book[j].startsWith(phone_book[i])) return false;
+                else break;
+            }
+        }
+        
+        return true;
+    }
+}
