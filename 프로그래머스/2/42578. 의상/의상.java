c3f135f6974aefaea6601e21d05fe2fac68fc3a9
HeeJun Yoon <unijun0109@gmail.com>
2025-01-19 03:26:59 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int result = 1;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
+        for (String[] cloth : clothes) {
+            if (map.containsKey(cloth[1])) {
+                map.replace(cloth[1], map.get(cloth[1]) + 1);
+            } else {
+                map.put(cloth[1], 1);
+            }
+        }
+        
+        if (map.size() == 1) {
+            return clothes.length;
+        }
+        
+        for (String key : map.keySet()) {
+            Integer value = map.get(key);
+            result = result * (value + 1);
+        }
+        
+        return result - 1;
+    }
+}
