52c067d23e58d167e78a4d8712e17121a51fbb8b
HeeJun Yoon <unijun0109@gmail.com>
2025-02-01 22:26:53 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,22 +3,19 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> list = new ArrayList<>();
-        int prev = (99 - progresses[0]) / speeds[0] + 1, cnt = 0;
+        int prev = (99 - progresses[0]) / speeds[0] + 1;
+        int cnt = 0;
         
         for (int i = 0; i < progresses.length; i++) {
             if ((99 - progresses[i]) / speeds[i] + 1 <= prev) {
-                System.out.println("if");
             } else {
                 list.add(cnt);
                 prev = (99 - progresses[i]) / speeds[i] + 1;
                 cnt = 0;
-                System.out.println("else");
             }
             cnt++;
         }
         list.add(cnt);
-        
-        System.out.println("리스트: " + list + "cnt: " + cnt);
         
         int[] answer = new int[list.size()];
         
@@ -29,3 +26,4 @@ class Solution {
         return answer;
     }
 }
+// 소요 시간: 26'01"
