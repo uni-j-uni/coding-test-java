f6933d01e650859f63dc4c199944d642658fafee
HeeJun Yoon <unijun0109@gmail.com>
2025-02-05 03:47:33 +0900

    [level 2] Title: 프로세스, Time: 1.28 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,37 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Collections;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> queue = new LinkedList<>();
+        int cnt = 0;
+        
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[]{i, priorities[i]});
+        }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int priority : priorities) {
+            priorityQueue.add(priority);
+        }
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            if (current[1] == priorityQueue.peek()) {
+                priorityQueue.poll();
+                cnt++;
+                
+                if (current[0] == location) {
+                    return cnt;
+                }
+            } else {
+                queue.add(current);
+            }
+        }
+        
+        return cnt;
+    }
+}
