d5af3904d56f236b433eb22cc1e7206b400032cb
HeeJun Yoon <unijun0109@gmail.com>
2025-02-05 03:09:14 +0900

    [level 2] Title: 주식가격, Time: 18.15 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for (int i = 0; i < prices.length - 1; i++) {
+            int sec = 1;
+            for (int j = i + 1; j < prices.length - 1; j++) {
+                if (prices[i] <= prices[j]) {
+                    sec++;
+                } else {
+                    break;
+                }
+            }
+            answer[i] = sec;
+        }
+        
+        return answer;
+    }
+}
