2c22bd152ec06b71efa35e5d92b293951157ae35
HeeJun Yoon <unijun0109@gmail.com>
2025-02-06 16:06:13 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
+        
         for (int i = 0; i < commands.length; i++) {
-            int[] result = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            Arrays.sort(result);
-            answer[i] = result[commands[i][2] - 1];
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
         }
+        
         return answer;
     }
 }
