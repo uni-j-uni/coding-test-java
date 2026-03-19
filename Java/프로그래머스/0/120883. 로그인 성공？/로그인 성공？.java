fe005f9da3e36ec5f4fad4b3a121c4d38ecd08a5
HeeJun Yoon <unijun0109@gmail.com>
2026-03-19 11:34:03 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        
+        for (String[] data : db) {
+            if (data[0].equals(id_pw[0])) {
+                if (data[1].equals(id_pw[1])) {
+                    return "login";
+                }
+                return "wrong pw";
+            }
+        }
+        
+        return "fail";
+    }
+}
